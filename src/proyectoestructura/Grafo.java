d2101e49adc8d54ed76de74f2d8475a6ff16d607
PC <PC@DESKTOP-5L0EOET>
2026-03-05 20:56:01 -0400

    comentacion de codigo del archivo de de grafo y correcion de la misma

--- src/proyectoestructura/Grafo.java
+++ src/proyectoestructura/Grafo.java
@@ -5,50 +5,62 @@
 package proyectoestructura;
 
 /**
- *
+ * Implementacion del grafo
+ * maneja todos los vertices y sus conexiones
  * @author PC
  */
 public class Grafo {
-    Vertice primero;
-    int t;
+    Vertice primero;        //primer vertices de la lista de vertice
+    int t;                  // Cantidad total de vertices
 
+    /**
+     * Contructor. crea un grafo vacio
+     */
     public Grafo() {
         this.primero = null;
         t = 0;
     }
 
+    /**
+     * Metodo que inserta un nuevo vertice aol grafo
+     * @param dato Nombre de la proteina
+     */
     public void insertar(String dato) {
         Vertice a = this.buscar(dato);
-        if (a != null) {
+        if (a != null) {            //si ya existe no lo inserta
             return;
         }
         Vertice nuevo = new Vertice(dato);
-        if (primero == null) {
+        if (primero == null) {          //si el grafo esta vacio inserta el primero
             primero = nuevo;
             t++;
         } else {
             Vertice aux = primero;
             while (aux.sig != null) {
-                aux = aux.sig;
+                aux = aux.sig;          //va al final del grafo
             }
-            aux.sig = nuevo;
-            nuevo.ant = aux.sig;
+            aux.sig = nuevo;            //Conecta el apuntador al nuevo
+            nuevo.ant = aux;
             t++;
         }
 
     }
 
+    /**
+     * Metodo que elimina el vertice del grafo
+     * @param dato Nombre de la porteina a eliminar
+     */
     public void eliminar(String dato) {
         if (primero != null) {
             Vertice aux = primero;
-            if (aux.proteina.equals(dato)) {
+            if (aux.proteina.equals(dato)) {        //Por si es el primero
                 primero = primero.sig;
                 primero.ant = null;
                 t--;
 
                 return;
             }
-            while (aux.sig != null && !aux.sig.proteina.equals(dato)) {
+            while (aux.sig != null && !aux.sig.proteina.equals(dato)) {     //busca el vertice a eliminar
                 aux = aux.sig;
             }
             if (aux.sig != null) {
@@ -57,7 +69,7 @@ public class Grafo {
                 t--;
             }
             aux = primero;
-            while (aux != null) {
+            while (aux != null) {                   // elimina todas las aristas que estaban conectada ese vertice
                 if (aux.aristas.primero.equals(dato)) {
                     aux.aristas.primero = aux.aristas.primero.sig;
                     aux.aristas.primero.ant = null;
@@ -76,7 +88,11 @@ public class Grafo {
 
         }
     }
-
+/**
+ * Busca el vertice por su nombre
+ * @param dato Nombre de la proteina a buscar
+ * @return Vertice encontrado o null por si no esta
+ */
     public Vertice buscar(String dato) {
         if (primero != null) {
             Vertice aux = primero;
@@ -88,7 +104,10 @@ public class Grafo {
         }
         return null;
     }
-
+/**
+ * Muestra todo el grafo
+ * @return la representacion del grafo en String
+ */
     public String mostrar() {
         String salida = "";
         Vertice aux = primero;
@@ -99,17 +118,24 @@ public class Grafo {
         }
         return salida;
     }
-
+/**
+ * Inserta una arista entre dos vertices
+ * @param a Vertice de oriigen
+ * @param b Vertice destino
+ * @param peso Peso de la arista
+ */
     public void insertarArista(Vertice a, Vertice b, int peso) {
         if (a != null && b != null) {
             b.aristas.insertar(a.proteina, peso);
             a.aristas.insertar(b.proteina, peso);
         }
         
-        
-    
     }
-    
+
+ /** 
+ *Encuentra el vertice con mas conexiones
+ * @return Vertice de mayor conexiones o mayor numero de aristas
+ */ 
     public Vertice mostrador_proteina(){
         int cont = 0;
         Vertice aux = primero;
@@ -130,6 +156,10 @@ public class Grafo {
         }
         return Mayorzote;
     }
+    /**
+     * Obtiene array con todos los vertices
+     * @return array de vertices
+     */
     public Vertice []getVertices(){
         Vertice[]x = new Vertice [this.t];
         Vertice aux = primero;
