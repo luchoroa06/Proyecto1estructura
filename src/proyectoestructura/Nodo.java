f9be975788b1ba6bd249f31e7e138a5f5371bbed
PC <PC@DESKTOP-5L0EOET>
2026-03-05 20:12:46 -0400

    comentacion de codigo del archivo de de Nodo

--- src/proyectoestructura/Nodo.java
+++ src/proyectoestructura/Nodo.java
@@ -5,15 +5,21 @@
 package proyectoestructura;
 
 /**
- *
- * @author PC
+ *  Representa un nodo en la listas
+ *  almacena una arista del grafo
  */
 public class Nodo {
-    Nodo sig;
-    Nodo ant;
-    String dato;
-    int peso;
+    Nodo sig;              //puntero al siguiente de las lista de aristas
+    Nodo ant;              //puntero del anterior de la lista de aristas
+    String dato;           //nombre de la nueva proteina
+    int peso;              //Peso de la arista
 
+    
+    /**
+     * contrustor para crear un nuevo nodo para una arista
+     * @param dato Nombre de la proteina
+     * @param peso Peso de la proteina
+     */
     public Nodo(String dato, int peso) {
         this.sig = null;
         this.ant = null;
