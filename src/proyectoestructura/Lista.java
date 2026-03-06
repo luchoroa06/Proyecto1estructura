d57907a84865752e5b9ac71b52be86a48058c885
PC <PC@DESKTOP-5L0EOET>
2026-03-05 20:37:38 -0400

    comentacion de codigo del archivo de de Lista y correcion de la misma

--- src/proyectoestructura/Lista.java
+++ src/proyectoestructura/Lista.java
@@ -6,40 +6,51 @@ package proyectoestructura;
 
 /**
  *
- * @author PC
+ * Implementaciones de los metodos de una lista doblemente enlazada que manejan las aristas de cada vertice
  */
 public class Lista {
 
-    Nodo primero;
-    int t;
-
+    Nodo primero;       //Primero nodo de la lista de aristas
+    int t;              // Tamaño de la lista
+/**
+ * constructor que crea la lista vacia
+ */
     public Lista() {
         this.primero = null;
         this.t = 0;
 
     }
+    /**
+     * metodo de insertar arista al final de la lista
+     * @param dato Nombre de la proteina
+     * @param peso peso de la proteina
+     */
 
     public void insertar(String dato, int peso) {
         Nodo nuevo = new Nodo(dato, peso);
-        if (primero == null) {
+        if (primero == null) {          //por si la lsita esta vacia
             primero = nuevo;
             t++;
         } else {
             Nodo aux = primero;
             while (aux.sig != null) {
-                aux = aux.sig;
+                aux = aux.sig;        //recorre hasta el final de la lista
             }
-            aux.sig = nuevo;
-            nuevo.ant = aux.sig;
+            aux.sig = nuevo;          // conecta el nuevo nodo
+            nuevo.ant = aux;
             t++;
         }
 
     }
+    /**
+     * Metodo que elimina una arista por su nombre
+     * @param dato Nombre de la proteina a eliminar
+     */
 
     public void eliminar(String dato) {
         if (primero != null) {
             Nodo aux = primero;
-            if (aux.dato.equals(dato)) {
+            if (aux.dato.equals(dato)) {        // Si es el primero
                 primero = primero.sig;
                 primero.ant = null;
                 t--;
@@ -47,21 +58,26 @@ public class Lista {
                 return;
             }
             while (aux.sig != null && !aux.sig.dato.equals(dato)) {
-                aux = aux.sig;
+                aux = aux.sig;                  //busca el nodo a eliminar
             }
             if (aux.sig != null) {
-                aux.sig = aux.sig.sig;
+                aux.sig = aux.sig.sig;          //reasigna los punteros 
                 aux.sig.ant = aux;
                 t--;
             }
 
         }
     }
+    /**
+     * Metodo de buscar una arista por su nombre de 
+     * @param dato Proteina a buscar
+     * @return retorna el nodo buscado o null por si no existe el nodo
+     */
 
     public Nodo buscar(String dato) {
         if (primero != null) {
             Nodo aux = primero;
-            while (aux != null && aux.dato.equals(dato)) {
+            while (aux != null && !aux.dato.equals(dato)) {
                 aux = aux.sig;
 
             }
@@ -70,6 +86,10 @@ public class Lista {
         return null;
     }
 
+    /**
+     * genera en string todas las aristas
+     * @return El string con todas las aristas 
+     */
     public String mostrar() {
         String salida = "";
         Nodo aux = primero;
@@ -80,6 +100,10 @@ public class Lista {
         }
         return salida; 
     }
+    /**
+     * Obtiene una array con todos los nombres de las proteinas
+     * @return Array de string con los nombres
+     */
     public String [] getNombres(){
             Nodo aux = primero;
             String [] x = new String[t];
